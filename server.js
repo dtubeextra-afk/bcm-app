22b0245b74fce08186eb714091a29e48337225eb
dtubeextra-afk <dtubeextra@gmail.com>
2026-06-06 11:17:08 +0000

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`SECURECODE running on port ${PORT}`);
 });
